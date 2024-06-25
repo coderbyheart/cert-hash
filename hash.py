48fb24a3d33d85d17c6b348edf54a47967eafccc
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-25 14:48:19 +0200

    fix: align hash with nRF Connect's Cellular Monitor's Device Manager

--- hash.py
+++ hash.py
@@ -4,6 +4,5 @@ import sys
 with open(sys.argv[1], 'rb') as file:
     cred_text = file.read()
 cred_text = cred_text.strip()
-cred_text = b"\n" + cred_text
 
 print(hashlib.sha256(cred_text).hexdigest().upper().strip(), end="")
