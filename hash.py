3ce9f4f3858825aadecc2112781a6de907c1a8cc
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-25 12:25:20 +0200

    test: add test

--- hash.py
+++ hash.py
@@ -5,4 +5,5 @@ with open(sys.argv[1], 'rb') as file:
     cred_text = file.read()
 cred_text = cred_text.strip()
 cred_text = b"\n" + cred_text
-print(hashlib.sha256(cred_text).hexdigest().upper())
+
+print(hashlib.sha256(cred_text).hexdigest().upper().strip(), end="")
